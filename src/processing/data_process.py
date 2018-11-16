6c5eb55dd05a4567a7289daf91743fa55fd342f5
Grégoire Lurton <gregoire.lurton@gmail.com>
2018-11-16 13:02:49 +0100

    updating source selection

--- src/processing/data_process.py
+++ src/processing/data_process.py
@@ -32,20 +32,21 @@ class measured_serie(object):
         # Question1 : we may not want to fill the gaps with not preferred values. leave it as a parameter.
         # Question2 : have benchmarking metrics
         """Build a unique data series from multiple data sources."""
+        sources = list(self.data_list.keys())
         full_time = []
-        for key in self.data_list.keys():
-            full_time = full_time + self.data_list[key]['monthly'].tolist()
-            if len(self.data_list[key]) > prefer_threshold * len(self.data_list[self.preferred_source]):
-                self.preferred_source = key
+        for source in sources:
+            full_time = full_time + self.data_list[source]['monthly'].tolist()
+            if len(sources) > prefer_threshold * len(self.data_list[self.preferred_source]):
+                self.preferred_source = source
         full_time = sorted(list(set(full_time)))
-        if len(self.data_list.keys()) == 1:
-            out = self.data_list[self.data_list.keys[0]]
-            out['source'] = self.data_list.keys[0]
-        if (self.preferred_source is not "") and (self.preferred_source in self.data_list.keys()):
+        if len(sources) == 1:
+            out = self.data_list[sources[0]]
+            out['source'] = sources[0]
+        if (self.preferred_source is not "") and (self.preferred_source in sources):
             out = self.data_list[self.preferred_source]
             out['source'] = self.preferred_source
         if (len(full_time) > len(out)) & (fill_gaps is True):
-            for source in list(self.data_list.keys()):
+            for source in sources:
                 remaining = [x for x in full_time if x not in out]
                 add_dat = self.data_list[source][self.data_list.monthly.isin(remaining)]
                 add_dat['source'] = source
@@ -54,8 +55,11 @@ class measured_serie(object):
 
     def impute_missing(self, full_range):
         """Imputes the number of patients for missing monthes of data"""
-        allmonths = range(full_range[0], full_range=1)
-        return s
+        allmonths = pd.date_range(full_range[0], full_range=1,
+                                  freq='MS').strftime("%Y-%b").tolist()
+        #self.preferred_serie.month =
+
+        return self
 
     def benchmark_serie(self):
         return self
