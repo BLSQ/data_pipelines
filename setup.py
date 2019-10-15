d45caa180c0621ee35f52bb723cc3866f56c64fc
Grégoire Lurton <gregoire.lurton@gmail.com>
2019-10-15 13:18:03 +0200

    taking out dotenv

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/BLSQ/data_pipelines",
     packages=["bluesquare_data_pipelines", "bluesquare_data_pipelines.access"],
-    install_requires=[
-          'dotenv',
-      ],
+#    install_requires=[
+#          'dotenv',
+#      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
