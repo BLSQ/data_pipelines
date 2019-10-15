ee71d391c926180e6e807dda19b2300cfaca3971
Grégoire Lurton <gregoire.lurton@gmail.com>
2019-10-15 13:39:30 +0200

    Adding geodata processing

---
+++ bluesquare_data_pipelines/access/geodata_process.py
@@ -0,0 +1,12 @@
+"""Read a DHIS database and prepare its content for analysis."""
+import geopandas as gpd
+
+def dhis_to_GeoDataFrame(dhis2_orgunits_table, level, type):
+        level_data = dhis2_orgunits_table[dhis2_orgunits_table.level == level]
+        if type == "point":
+            level_data.loc[:,"coordinates"] = level_data.coordinates.astype(str)
+            level_data.loc[:,"lat"] = level_data.loc[:,"coordinates"].str.split("\[|\]|,").apply(lambda x: float(x[1]) if len(x) == 4 else np.nan)
+            level_data.loc[:,"lon"] = level_data.loc[:,"coordinates"].str.split("\[|\]|,").apply(lambda x: float(x[2]) if len(x) == 4 else np.nan)
+        geodataframe = gpd.points_from_xy(level_data.lat,
+                                          level_data.lon),crs={'init':'epsg:4326'})
+        return geodataframe
