1b16d770061c8256adfabc770a3de1b5e7e4c537
Grégoire Lurton <gregoire.lurton@gmail.com>
2019-10-15 13:56:23 +0200

    Something more ... debugging

--- bluesquare_data_pipelines/access/geodata_process.py
+++ bluesquare_data_pipelines/access/geodata_process.py
@@ -7,6 +7,6 @@ def dhis_to_GeoDataFrame(dhis2_orgunits_table, level, type):
             level_data.loc[:,"coordinates"] = level_data.coordinates.astype(str)
             level_data.loc[:,"lat"] = level_data.loc[:,"coordinates"].str.split("\[|\]|,").apply(lambda x: float(x[1]) if len(x) == 4 else np.nan)
             level_data.loc[:,"lon"] = level_data.loc[:,"coordinates"].str.split("\[|\]|,").apply(lambda x: float(x[2]) if len(x) == 4 else np.nan)
-        data = level_data.drop(["lat","lon","coordinates"])
+        data = level_data.drop(["lat","lon","coordinates"], axis=1)
         geodataframe = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(level_data.lat, level_data.lon) ,crs={'init':'epsg:4326'})
         return geodataframe
