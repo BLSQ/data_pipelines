3c40b128c892b7e21e01869a5717be23cfaf8a32
Grégoire Lurton <gregoire.lurton@gmail.com>
2019-10-15 13:42:02 +0200

    Add geodata_process as package

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BLSQ/data_pipelines",
-    packages=["bluesquare_data_pipelines", "bluesquare_data_pipelines.access"],
+    packages=["bluesquare_data_pipelines", "bluesquare_data_pipelines.access", "bluesquare_data_pipelines.geodata_process"],
     install_requires=[
           'python-dotenv',
       ],
