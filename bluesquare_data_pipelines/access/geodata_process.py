77b08e6137a50b4eaf6a723b14529e09174a286d
Grégoire Lurton <gregoire.lurton@gmail.com>
2019-10-15 14:07:03 +0200

    Adding dependcy

--- bluesquare_data_pipelines/access/geodata_process.py
+++ bluesquare_data_pipelines/access/geodata_process.py
@@ -1,5 +1,6 @@
 """Read a DHIS database and prepare its content for analysis."""
 import geopandas as gpd
+import numpy as np
 
 def dhis_to_GeoDataFrame(dhis2_orgunits_table, level, type):
         level_data = dhis2_orgunits_table[dhis2_orgunits_table.level == level]
