7cdb78878e34a7cf732851c910e8ff7f8948efae
Grégoire Lurton <gregoire.lurton@gmail.com>
2019-10-17 21:28:45 +0200

    Adding Shapely dependency

--- bluesquare_data_pipelines/access/geodata_process.py
+++ bluesquare_data_pipelines/access/geodata_process.py
@@ -1,6 +1,7 @@
 """Read a DHIS database and prepare its content for analysis."""
 import geopandas as gpd
 import numpy as np
+from shapely.geometry import LineString, Polygon, Point
 
 def dhis_to_GeoDataFrame(dhis2_orgunits_table, level, type_shape):
         level_data = dhis2_orgunits_table[dhis2_orgunits_table.level == level]
